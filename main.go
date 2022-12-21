fa6304a6b12b20881526ff6acf199deb77aff8f8
M.Schirmer <fain.marcel@gmail.com>
2022-12-21 15:11:44 +0100

    #6 - Show warning when soft limit is exceeded

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"rockstaedt/commit-message-check/src/utils"
 )
 
+const (
+	softLimit = 50
+	hardLimit = 72
+)
+
 func main() {
 	log.Println("[INFO]\t Validating commit message...")
 	commitLines, err := utils.GetLinesFromTextFile(os.Args[1])
@@ -17,13 +22,20 @@ func main() {
 	cm, err := src.CreateCommitMessageFrom(commitLines)
 	if err != nil {
 		log.Printf("[ERROR]\t Could not create object: %q", err.Error())
-		os.Exit(1)
+		os.Exit(2)
 	}
 
-	if cm.ValidateSubject() == false {
-		log.Println("[ERROR]\t Abort commit. Subject line too long. Please fix.")
-		os.Exit(1)
+	numOfExceedingChars := cm.ValidateSubject()
+	if numOfExceedingChars == 0 {
+		os.Exit(0)
 	}
+
+	if numOfExceedingChars > (hardLimit - softLimit) {
+		log.Println("[ERROR]\t Abort commit. Subject line too long. Please fix.")
+		os.Exit(3)
+	}
+
+	log.Printf("[WARN]\t Your subject exceeds the soft limit of 50 chars by %d chars.", numOfExceedingChars)
 
 	os.Exit(0)
 }
