4b9f9e629bde44e2e13cd12d0fa854d3af8c6c14
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2023-01-30 18:03:10 +0100

    Implement happy path for setup

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,1 +1,22 @@
 package cmd
+
+import "log"
+
+func Setup(files []string) int {
+	if hasGitRepository(files) == false {
+		log.Println("[ERROR]\t No git repository could be found.")
+		return 1
+	}
+
+	return 0
+}
+
+func hasGitRepository(files []string) bool {
+	for _, file := range files {
+		if file == ".git" {
+			return true
+		}
+	}
+
+	return false
+}
--- cmd/setup_test.go
+++ cmd/setup_test.go
@@ -1,1 +1,42 @@
 package cmd
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
+func TestSetup(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	log.SetOutput(buffer)
+
+	t.Run("returns 0 and init hook script", func(t *testing.T) {
+		status := Setup([]string{"src", "internal", ".git"})
+
+		assert.Equal(t, status, 0)
+	})
+
+	t.Run("returns 1 if git repo is not initialized and logs it", func(t *testing.T) {
+		buffer.Reset()
+		status := Setup([]string{"src", "internal"})
+
+		assert.Equal(t, status, 1)
+		assert.Contains(t, buffer.String(), "[ERROR]\t No git repository could be found.")
+	})
+}
+
+func TestHasGitRepository(t *testing.T) {
+
+	t.Run("returns true .git dir could be found", func(t *testing.T) {
+		got := hasGitRepository([]string{"tmp", "src", ".git"})
+
+		assert.True(t, got)
+	})
+
+	t.Run("return false when no git repository could be found", func(t *testing.T) {
+		got := hasGitRepository([]string{"tmp", "src"})
+
+		assert.False(t, got)
+	})
+}
