f18951ee760f2602a95c2649b09c72770edd687f
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2023-02-05 11:53:55 +0100

    Let uninstall remove commit-msg

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -1,5 +1,10 @@
 package cmd
 
+import "rockstaedt/commit-message-check/util"
+
 func Uninstall(gitPath string) int {
+
+	_ = util.WalkHookDirs(gitPath, util.DeleteHook)
+
 	return 0
 }
--- cmd/uninstall_test.go
+++ cmd/uninstall_test.go
@@ -1,11 +1,36 @@
 package cmd
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
 
 func TestUninstall(t *testing.T) {
 
+	createDirs := func() string {
+		path := t.TempDir()
+		err := os.Mkdir(fmt.Sprintf("%s/hooks", path), os.ModePerm)
+		assert.Nil(t, err)
+		_, err = os.Create(fmt.Sprintf("%s/hooks/commit-msg", path))
+		assert.Nil(t, err)
+		err = os.Mkdir(fmt.Sprintf("%s/xyz", path), os.ModePerm)
+		assert.Nil(t, err)
+		_, err = os.Create(fmt.Sprintf("%s/xyz/commit-msg", path))
+		assert.Nil(t, err)
+
+		return path
+	}
+
 	t.Run("returns 0 and removes all occurrences of commit-msg", func(t *testing.T) {
-		_ = Uninstall("")
+		path := createDirs()
+
+		status := Uninstall(path)
+
+		assert.Equal(t, 0, status)
+		assert.NoFileExists(t, fmt.Sprintf("%s/hooks/commit-msg", path))
+		assert.FileExists(t, fmt.Sprintf("%s/xyz/commit-msg", path))
 	})
 
 	t.Run("returns 1 on any error", func(t *testing.T) {
