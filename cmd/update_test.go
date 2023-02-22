7457b5878511643e13b9bae0cbad904e58d7c8f0
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2023-02-22 18:13:08 +0100

    Clean up tests

--- cmd/update_test.go
+++ cmd/update_test.go
@@ -9,25 +9,26 @@ import (
 
 func TestUpdate(t *testing.T) {
 
-	t.Run("returns 0 and", func(t *testing.T) {
+	t.Run("returns 0 and no message when local version is latest", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		}))
+		defer ts.Close()
 
-		t.Run("requests latest release version", func(t *testing.T) {
-			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			}))
-			defer ts.Close()
+		status := Update("v1.0.0", ts.URL)
 
-			status := Update("v1.0.0", ts.URL)
+		assert.Equal(t, 0, status)
+	})
 
-			assert.Equal(t, 0, status)
-		})
+	t.Run("--compares latest version with binary version", func(t *testing.T) {
+		t.Skip()
+	})
 
-		t.Run("compares latest version with binary version", func(t *testing.T) {
-			t.Skip()
-		})
+	t.Run("--downloads install script if newer version available", func(t *testing.T) {
+		t.Skip()
+	})
 
-		t.Run("downloads install script if newer version available", func(t *testing.T) {
-			t.Skip()
-		})
+	t.Run("returns 1 and a message when a newer version was found", func(t *testing.T) {
+		t.Skip()
 	})
 
 	t.Run("returns 1 on any error", func(t *testing.T) {
