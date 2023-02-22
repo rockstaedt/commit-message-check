fc22eae175aa935025f46fe10a0c7a2c74eed75d
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2023-02-22 18:24:35 +0100

    Make request and compare versions

--- cmd/update.go
+++ cmd/update.go
@@ -1,5 +1,23 @@
 package cmd
 
-func Update(_, _ string) int {
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type responseData struct {
+	TagName string `json:"tag_name"`
+}
+
+func Update(version, url string) int {
+	res, _ := http.Get(url)
+
+	var data responseData
+	_ = json.NewDecoder(res.Body).Decode(&data)
+
+	if version != data.TagName {
+		return 1
+	}
+
 	return 0
 }
--- cmd/update_test.go
+++ cmd/update_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -9,8 +10,11 @@ import (
 
 func TestUpdate(t *testing.T) {
 
-	t.Run("returns 0 and no message when local version is latest", func(t *testing.T) {
+	t.Run("returns 0 and a message when local version is latest", func(t *testing.T) {
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			_, err := fmt.Fprintln(w, `{"tag_name":"v1.0.0"}`)
+			assert.Nil(t, err)
 		}))
 		defer ts.Close()
 
@@ -19,16 +23,21 @@ func TestUpdate(t *testing.T) {
 		assert.Equal(t, 0, status)
 	})
 
-	t.Run("--compares latest version with binary version", func(t *testing.T) {
-		t.Skip()
-	})
-
 	t.Run("--downloads install script if newer version available", func(t *testing.T) {
 		t.Skip()
 	})
 
-	t.Run("returns 1 and a message when a newer version was found", func(t *testing.T) {
-		t.Skip()
+	t.Run("returns 1 and no message when a newer version was found", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			_, err := fmt.Fprintln(w, `{"tag_name":"v1.2.0"}`)
+			assert.Nil(t, err)
+		}))
+		defer ts.Close()
+
+		status := Update("v1.0.0", ts.URL)
+
+		assert.Equal(t, 1, status)
 	})
 
 	t.Run("returns 1 on any error", func(t *testing.T) {
