85fcda510a9b327682b1ec0298de827b442f19d5
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2023-04-02 18:09:13 +0200

    Define test cases for new handler

---
+++ cmd/handler_test.go
@@ -0,0 +1,26 @@
+package cmd
+
+import "testing"
+
+func TestHandler(t *testing.T) {
+
+	t.Run("executes uninstall command", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("executes setup command", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("executes update command", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("executes validate command", func(t *testing.T) {
+		t.Skip()
+	})
+
+	t.Run("prints warning when any other command", func(t *testing.T) {
+		t.Skip()
+	})
+}
